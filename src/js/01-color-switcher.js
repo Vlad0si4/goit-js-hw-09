df04c37f0f5bc1c4d09659bb59c43e058123b5f6
Vlad0si4 <boiko.vlad91@gmail.com>
2023-05-30 16:01:01 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  const timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+  }, 1000);
+
+  buttonStop.addEventListener('click', () => {
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId)
+})
+})
 
