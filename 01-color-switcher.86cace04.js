306fd2cf2937b25ee33a0eeb53acb8b9ed94d289
Vlad0si4 <122666761+Vlad0si4@users.noreply.github.com>
2023-06-01 12:47:38 +0000

    Deploying to gh-pages from @ Vlad0si4/goit-js-hw-09@904ac540e5c7c8a39c4dbe4869fc4517ffd142f9 🚀

--- 01-color-switcher.86cace04.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(()=>{const a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0,t.disabled=!1}),1e3);t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(a)}))}));
-//# sourceMappingURL=01-color-switcher.86cace04.js.map
